937dcd408e5f867dba09f5f23e12969da8b1b38a
Miki Shimura <miki.shimura.ves@gmail.com>
2024-06-22 23:20:51 +0200

    Update admin credencial

--- bwctest/src/configs/generic_configs.py
+++ bwctest/src/configs/generic_configs.py
@@ -1,6 +1,6 @@
 class GenericConfigs:
-    ADMIN_NAME = "Miki"
-    ADMIN_PASS = "Miki"
+    ADMIN_NAME = "Admin"
+    ADMIN_PASS = "Admin"
 
     LOGIN_SUCCESS_MSG = "Welcome back!"
     LOGIN_INVAILD_CREDENCIAL_ERR_MSG = "Password or username is incorrect"
