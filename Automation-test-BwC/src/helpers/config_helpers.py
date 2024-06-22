4acdd0676ea9e637a3c8dfb88b2983ed0ced9d76
Miki Shimura <miki.shimura.ves@gmail.com>
2024-06-22 14:35:26 +0200

    Add get_base_url() function

---
+++ Automation-test-BwC/src/helpers/config_helpers.py
@@ -0,0 +1,11 @@
+import os
+
+def get_base_url():
+    env = os.environ.get("ENV", "prod")
+
+    if env.lower() == "test":
+        return "http://localhost:7888/Belgrade-with-children"
+    elif env.lower() == "prod":
+        return "https://belgrade-with-kids.onrender.com/"
+    else:
+        raise Exception(f"Unknown envirnment: {env}")
