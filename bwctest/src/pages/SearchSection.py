94d02f1e87d621b8c5c9b81aea5b139687bcdf16
Miki Shimura <miki.shimura.ves@gmail.com>
2024-07-30 10:14:24 +0200

    Improve tcid318 to choose criteria randomly

--- bwctest/src/pages/SearchSection.py
+++ bwctest/src/pages/SearchSection.py
@@ -1,5 +1,6 @@
 from src.SeleniumExtended import SeleniumExtended
 from src.pages.locators.SearchSectionLocators import SearchSectionLocators
+import random
 
 class SearchSection(SearchSectionLocators):
 
@@ -13,27 +14,21 @@ class SearchSection(SearchSectionLocators):
                       self.CATEGORY_CHECKBOX_EDUCATION]
         
         if categories > 5:
-            print("Argument of function 'choose_categories_on_seach_section' should be lower than 6")
-        for n in range(categories): 
-            self.sl.wait_and_click(checkboxes[n])
-        
-        # I want to choose category random but code below doesn't work. Consider later.
-        # if categories > 5:
-        #     print("Argument of function 'choose_ages_on_seach_section' should be lower than 6")
-        # else:
-        #     rand_categories = random.sample(checkboxes, categories)
-        #     print(rand_categories)
-        #     for i in rand_categories: 
-        #         print(i)
-        #         self.sl.wait_and_click(i[0], i[1])
+            print("Argument of function 'choose_ages_on_seach_section' should be lower than 6")
+        else:
+            rand_categories = random.sample(checkboxes, categories)
+            for i in rand_categories: 
+                self.driver.find_element(i[0], i[1]).click()
 
     def choose_ages_on_seach_section(self, ages=2):
         checkboxes = [self.AGES_CHECKBOX_0_1, self.AGES_CHECKBOX_2_3, self.AGES_CHECKBOX_4_6, 
                       self.AGES_CHECKBOX_7_9, self.AGES_CHECKBOX_10]
         if ages > 5:
             print("Argument of function 'choose_ages_on_seach_section' should be lower than 6")
-        for n in range(ages): 
-            self.sl.wait_and_click(checkboxes[n])
+        else:
+            rand_ages = random.sample(checkboxes, ages)
+            for i in rand_ages: 
+                self.driver.find_element(i[0], i[1]).click()
 
     def click_search_button(self):
         self.sl.wait_and_click(self.SEARCH_BTN)
